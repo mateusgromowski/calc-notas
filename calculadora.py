8ce6d3faedf6a72188b717a015b19423a9678694
Mateus Barbosa Gromowski <mateusbarbosagromowski@gmail.com>
2026-05-14 11:02:34 -0300

    feat: adicionada a função de cálculo da nota final pós exame

--- calculadora.py
+++ calculadora.py
@@ -2,6 +2,8 @@ NOTA_MIN = 0.0
 NOTA_MAX = 10.0
 MEDIA_APROVACAO = 7.0
 MEDIA_REPROVACAO = 4.0
+MEDIA_APROVACAO_RECUPERACAO = 6.0
+
 def situacao(media):
     if media >= MEDIA_APROVACAO:
         return "aprovado"
@@ -17,3 +19,13 @@ def calcula_media(n1, n2, n3):
     for nota in (n1, n2, n3):
         _valida(nota)
     return (n1 + n2 + 2 * n3) / 4
+
+def situacao_final(media, nota_recuperacao):
+    _valida(media)
+    _valida(nota_recuperacao)
+
+    media_final = ( media + nota_recuperacao ) / 2
+
+    if media_final >= MEDIA_APROVACAO_RECUPERACAO:
+        return "aprovado na recuperacao"
+    return "reprovado na recuperacao"
